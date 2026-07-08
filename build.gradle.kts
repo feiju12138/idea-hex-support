64436ec72efe389786be73d4aadbcc65cf2b230e
feiju12138 <49089085+feiju12138@users.noreply.github.com>
2026-07-08 21:04:13 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij.platform")
+}
+
+group = "cn.fj.loli"
+version = "1.0.0"
+
+dependencies {
+    intellijPlatform {
+        intellijIdea("2026.1")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "cn.fj.loli.hexsupport"
+        name = "Hex Support"
+        version = project.version.toString()
+        description = """
+            <p>Open and edit files as hexadecimal data inside IntelliJ IDEA.</p>
+            <p>Provides an editable hex byte table with offset column, raw data preview, hex/ASCII pattern search, offset navigation, multi-selection editing, undo/redo, and fragment import/export.</p>
+        """.trimIndent()
+        changeNotes = """
+            <ul>
+                <li>1.0.0: Initial release.</li>
+            </ul>
+        """.trimIndent()
+        vendor {
+            name = "feiju12138"
+            url = "https://github.com/feiju12138/idea-hex-support"
+        }
+        ideaVersion {
+            sinceBuild = "232"
+            untilBuild = "269.*"
+        }
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+        options.encoding = "UTF-8"
+    }
+
+    named("buildSearchableOptions") {
+        enabled = false
+    }
+}
